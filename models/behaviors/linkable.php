c60847aea1e73c6a8015ee38d6523beefc43f052
An M <anmichel@gmail.com>
2012-04-18 22:21:26 -0430

    removing again ['fields'] check

--- models/behaviors/linkable.php
+++ models/behaviors/linkable.php
@@ -146,7 +146,7 @@ class LinkableBehavior extends ModelBehavior {
 					}
 
 					//do not mess with fields if specified in $query
-					if (empty($query['fields'])) {
+					//if (empty($query['fields'])) {
 						if (!empty($options['fields'])) {
 							if ($options['fields'] === true && !empty($association['fields'])) {
 								$options['fields'] = $db->fields($_Model, null, $association['fields']);
@@ -184,7 +184,7 @@ class LinkableBehavior extends ModelBehavior {
 								$query['fields'] = array_merge($db->fields($Model), $options['fields']);
 							}
 						}
-					}
+					//}
 
 					$options[$this->_key] = am($options[$this->_key], array_diff_key($options, $optionsKeys));
 					$options = array_intersect_key($options, $optionsKeys);
